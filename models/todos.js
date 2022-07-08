607957300dd20c54ddbf8dc4258c3d70fc4ceeed
joehinmoro <joehinmoro@gmail.com>
2022-07-08 07:27:02 +0100

    exported todo model

--- models/todos.js
+++ models/todos.js
@@ -1,9 +1,35 @@
 // IMPORTS
-const express = require("express");
-const router = express.Router();
+const mongoose = require("mongoose");
+const { Schema: schema } = mongoose;
+// const schema = mongoose.Schema
 
-// ROUTES
-// 1. Add (CREATE)
-router.post();
+// mongoDB connection
+mongoose
+    .connect("mongodb://localhost:27017/i4gxzTodosDB")
+    .then(() => {
+        console.log("DB connection success");
+    })
+    .catch((err) => {
+        console.log("DB connection error:", err);
+    });
 
-//
+// Todo schema
+const todoSchema = new schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+// Todo Model
+const Todo = mongoose.Model("Todo", todoSchema);
+
+// EXPORTS
+module.exports = Todo;
